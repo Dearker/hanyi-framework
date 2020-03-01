d540d1e5f72e852bb33530045946a153c874ab6e
Dearker <1823268262@qq.com>
2020-03-01 23:08:29 +0800

    custom framework

--- src/main/java/com/hanyi/framework/mybatis/SqlSession.java
+++ src/main/java/com/hanyi/framework/mybatis/SqlSession.java
@@ -14,7 +14,7 @@ public class SqlSession {
 	 * @param <T>
 	 * @return
 	 */
-	public static <T> T getMapper(Class classz) {
+	public static <T> T getMapper(Class<T> classz) {
 		return (T) Proxy.newProxyInstance(classz.getClassLoader(),
 				                          new Class[] { classz },
 				                          new ExtInvocationHandlerMbatis(classz));
